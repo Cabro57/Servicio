07598499502888e87acab2c4e4d9ca803325f1a0
Cabro <ozen.samet.57@gmail.com>
2025-09-06 22:42:02 +0300

    refactor: remove legacy CSV import system
    - Deleted ImportManager, CsvImporter, CsvImporterResult, ImporterUI, CsvMapper classes
    - Cleaned up unused CSV import logic and related references

--- src/main/java/tr/cabro/servicio/application/listeners/WindowClosingEvent.java
+++ src/main/java/tr/cabro/servicio/application/listeners/WindowClosingEvent.java
@@ -1,7 +1,6 @@
 package tr.cabro.servicio.application.listeners;
 
 import tr.cabro.servicio.Servicio;
-import tr.cabro.servicio.application.ui.ImporterUI;
 
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -9,16 +8,7 @@ import java.awt.event.WindowListener;
 public class WindowClosingEvent implements WindowListener {
     @Override
     public void windowOpened(WindowEvent e) {
-        if (Servicio.getSettings().isFirstRun()) {
-            ImporterUI dialog = new ImporterUI();
 
-            if (dialog.hasDetectedFolder()) {
-                dialog.setVisible(true);
-            }
-
-            Servicio.getSettings().setFirstRun(false);
-            Servicio.getSettings().save();
-        }
     }
 
     @Override
